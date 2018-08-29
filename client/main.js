cc3be1879e7822bed845f8375257a216fee13018
TheBunanna <thebunanna@gmail.com>
2018-08-28 23:17:02 -0500

    should work

--- client/main.js
+++ client/main.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from '../imports/src/App';
 import {Meteor} from 'meteor/meteor';
-import {render} from 'react-dom';
+//import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import configureStore from '../imports/src/redux/store';
+import {Provider} from 'react-redux';
 
-import App from '../imports/containers/App.jsx';
+let initialState = {
+    count:0,
+    menu: false,
+}
+let store = configureStore(initialState);
 
+class Routing extends React.Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <App />
+            </Provider>
+        )
+    }
+}
 Meteor.startup(() => {
-  render(<App />, document.getElementById('root'))
-});
+    ReactDOM.render(<Routing />, document.getElementById('root'));
+})
